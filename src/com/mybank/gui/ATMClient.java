ecb4a89a6d9d801f4febf57537e6d02f6b54c45b
Pick <o.nekvinda@gmail.com>
2014-12-14 16:09:22 +0100

    cv10.2

--- src/com/mybank/gui/ATMClient.java
+++ src/com/mybank/gui/ATMClient.java
@@ -1,14 +1,15 @@
 package com.mybank.gui;
 
-import com.mybank.domain.*;
 import com.mybank.data.*;
 import java.io.*;
 import java.awt.*;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class ATMClient {
 
@@ -41,24 +42,85 @@ public class ATMClient {
         }
     }
 
-  // PLACE YOUR GUI CODE HERE
+    // PLACE YOUR GUI CODE HERE
+    JFrame f;
+    JPanel left, leftTop, leftMid, leftBot, center;
+    JTextArea outputTextArea;
+    JScrollPane outputScrollPane;
+    JButton displayAccBalance, makeDeposit, makeWithdrawal;
+    JTextField dataEntry, message;
+    JButton[] keyPadBtn;
+    GridBagConstraints c = new GridBagConstraints();
+
     private void launchFrame() {
-        JFrame f = new JFrame("First Java Bank ATM");
+        f = new JFrame("First Java Bank ATM");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        
-        JPanel left = new JPanel(new GridLayout(5, 1));
-        JButton 
-        
-        
-        
-        JTextArea textarea = new JTextArea("");
-        JScrollPane scroll = new JScrollPane(textarea);
-        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        left = new JPanel();
+        left.setLayout(new GridBagLayout());
 
-        f.add(scroll, BorderLayout.CENTER);
-        
-        f.setSize(500, 300);
+        leftTop = new JPanel(new GridLayout(3, 1));
+        displayAccBalance = new JButton("Display account balance");
+        makeDeposit = new JButton("Make a deposit");
+        makeWithdrawal = new JButton("Make a withdrawal");
+        leftTop.add(displayAccBalance);
+        leftTop.add(makeDeposit);
+        leftTop.add(makeWithdrawal);
+
+        dataEntry = new JTextField(10);
+        dataEntry.setEditable(false);
+
+        leftBot = new JPanel(new GridLayout(4, 3)); // 
+        keyPadBtn = new JButton[12];
+        for (int i = 0; i < keyPadBtn.length; i++) {
+            String name;
+            switch (i) {
+                case 9:
+                    name = "0";
+                    break;
+                case 10:
+                    name = "";
+                    break;
+                case 11:
+                    name = "Enter";
+                    break;
+                default:
+                    name = "" + (i + 1);
+            }
+
+            keyPadBtn[i] = new JButton(name);
+            keyPadBtn[i].setPreferredSize(new Dimension(60, 30));
+            keyPadBtn[i].setMargin(new Insets(0, 0, 0, 0));
+            leftBot.add(keyPadBtn[i]);
+        }
+
+        center = new JPanel();
+        center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));
+        outputTextArea = new JTextArea(10, 75);
+        outputTextArea.setEditable(false);
+        outputScrollPane = new JScrollPane(outputTextArea);
+        outputScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        message = new JTextField(75);
+        center.add(outputScrollPane);
+        center.add(message);
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        left.add(leftTop, c);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        left.add(dataEntry, c);
+
+        c.gridx = 0;
+        c.gridy = 2;
+        left.add(leftBot, c);
+
+        f.add(left, BorderLayout.WEST);
+        f.add(center, BorderLayout.CENTER);
+
+        f.pack();
         f.setVisible(true);
     }
 }
